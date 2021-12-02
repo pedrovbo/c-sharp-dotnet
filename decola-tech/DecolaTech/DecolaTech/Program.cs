609f4566c9296936273b41a5cc369083bdd96554
pedrovbo <pedrovicboliveira@gmail.com>
2021-12-02 13:51:26 -0300

    add more C# studies

---
+++ decola-tech/DecolaTech/DecolaTech/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DecolaTech;
+
+namespace DecolaTech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa person = new Pessoa();
+
+            person.Nome = "Pedro";
+            person.Idade = 32;
+            person.Estado = "Recife";
+
+            var person2 = new Pessoa();
+
+            person2.Nome = "Géssica";
+            person2.Idade = 32;
+            person2.Estado = "Recife";
+
+            Console.WriteLine(person2);
+            Console.WriteLine(person);
+        }
+    }
+}
