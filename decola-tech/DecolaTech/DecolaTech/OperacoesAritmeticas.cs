704d585d0166678a955a5fe879f83f21087af366
pedrovbo <pedrovicboliveira@gmail.com>
2021-12-02 02:41:41 -0300

    add some test studies

---
+++ decola-tech/DecolaTech/DecolaTech/OperacoesAritmeticas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacoesAritmeticas
+{
+    public class OperacoesAritmeticas
+    {
+        public int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtracao(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Divisao(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Multiplicacao(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ decola-tech/DecolaTech/DecolaTechTests/OperacoesAritmeticasTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OperacoesAritmeticas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacoesAritmeticas.Tests
+{
+    [TestClass()]
+    public class OperacoesAritmeticasTests
+    {
+        [TestMethod()]
+        public void SomaTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 10;
+            var operacao = new OperacoesAritmeticas();
+
+            var result = operacao.Soma(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void SubtracaoTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 0;
+            var operacao = new OperacoesAritmeticas();
+
+            var result = operacao.Subtracao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void DivisaoTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 1;
+            var operacao = new OperacoesAritmeticas();
+
+            var result = operacao.Divisao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void MultiplicacaoTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 25;
+            var operacao = new OperacoesAritmeticas();
+
+            var result = operacao.Multiplicacao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+    }
+}
