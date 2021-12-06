1ae9d4a400ca74dd8984fc0c70b11654fea82794
pedrovbo <pedrovicboliveira@gmail.com>
2021-12-06 16:23:32 -0300

    add OOP studies

---
+++ decola-tech/POO/ExemploPOO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            p1.Nome = "Pedro";
+            p1.Idade = 32;
+
+            p1.Apresentar();
+        }
+    }
+}
