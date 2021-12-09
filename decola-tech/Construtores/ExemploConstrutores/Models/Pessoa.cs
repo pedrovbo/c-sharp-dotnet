858d4343e64af9b95173e9b284dff03d765a6a0d
pedrovbo <pedrovicboliveira@gmail.com>
2021-12-09 01:56:05 -0300

    add more constructor studies

--- decola-tech/Construtores/ExemploConstrutores/Models/Pessoa.cs
+++ decola-tech/Construtores/ExemploConstrutores/Models/Pessoa.cs
@@ -5,16 +5,17 @@ namespace ExemploConstrutores.Models
         private string nome;
         private string sobrenome;
 
-        public Pessoa()
-        {
-            nome = string.Empty;
-            sobrenome = string.Empty;
-        }
+        // public Pessoa()
+        // {
+        //     nome = string.Empty;
+        //     sobrenome = string.Empty;
+        // }
 
         public Pessoa(string nome, string sobrenome)
         {
             this.nome = nome;
             this.sobrenome = sobrenome;
+            System.Console.WriteLine("Construtor classe pessoa!");
         }
 
         public void Apresentar()
--- decola-tech/Construtores/ExemploConstrutores/Program.cs
+++ decola-tech/Construtores/ExemploConstrutores/Program.cs
@@ -7,7 +7,17 @@ namespace ExemploConstrutores
     {
         static void Main(string[] args)
         {
-            Log log = new Log();
+
+            Aluno aluno = new Aluno("Pedro", "Oliveira", "C# e .NET");
+            aluno.Apresentar();
+            // Log log = Log.GetInstance();
+
+            // log.PropriedadeLog = "Teste instância";
+
+            // Log log2 = Log.GetInstance();
+            // System.Console.WriteLine(log2.PropriedadeLog);
+
+            // Log log = new Log();
             
             // Pessoa p1 = new Pessoa("Pedro", "Oliveira");
             // p1.Apresentar();
