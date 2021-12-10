76be92f80bb5f94b9a4f5fa0ab42f633c9fa8dc6
pedrovbo <pedrovicboliveira@gmail.com>
2021-12-10 17:22:44 -0300

    add arrays study

--- decola-tech/ExemploColecoes/Colecoes/Program.cs
+++ decola-tech/ExemploColecoes/Colecoes/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayInteiros = new int[3];
+
+            arrayInteiros[0] = 10;
+            arrayInteiros[1] = 20;
+            arrayInteiros[2] = 30;
+
+            System.Console.WriteLine("Percorrendo o array pelo For");
+            for (int i = 0; i < arrayInteiros.Length; i++)
+            {
+                System.Console.WriteLine(arrayInteiros[i]);
+            }
+
+            System.Console.WriteLine("Percorrendo o array pelo ForEach");
+            foreach (int item in arrayInteiros)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
