8667b5e954fc9849657a9e614df29fd0ad794e3b
pedrovbo <pedrovicboliveira@gmail.com>
2021-12-10 23:15:50 -0300

    add bubble sort study

--- decola-tech/ExemploColecoes/Colecoes/Program.cs
+++ decola-tech/ExemploColecoes/Colecoes/Program.cs
@@ -1,26 +1,43 @@
 ﻿using System;
 
+using Colecoes.Helper;
+
 namespace Colecoes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[,] matriz = new int[4, 2]
-            {
-                {8 , 8},
-                {10, 20}, 
-                {50, 100}, 
-                {90, 200}
-            };
+            OperacoesArray op = new OperacoesArray();
+            int[] array = new int[5] { 6, 3, 8, 1, 9};
 
-            for (int i = 0; i < matriz.GetLength(0); i++)
-            {
-                for (int j = 0; j < matriz.GetLength(1); j++)
-                {
-                    System.Console.WriteLine(matriz[i, j]);
-                }
-            }
+            System.Console.WriteLine("Array original: ");
+            op.ImprimirArray(array);
+
+            op.OrdernarBubbleSort(ref array);
+
+            System.Console.WriteLine("Array ordenado:");
+            op.ImprimirArray(array);
+
+
+            
+            // int[,] matriz = new int[4, 2]
+            // {
+            //     {8 , 8},
+            //     {10, 20}, 
+            //     {50, 100}, 
+            //     {90, 200}
+            // };
+
+            // for (int i = 0; i < matriz.GetLength(0); i++)
+            // {
+            //     for (int j = 0; j < matriz.GetLength(1); j++)
+            //     {
+            //         System.Console.WriteLine(matriz[i, j]);
+            //     }
+            // }
+
+
 
             // matriz[0, 0] = 5;
 
