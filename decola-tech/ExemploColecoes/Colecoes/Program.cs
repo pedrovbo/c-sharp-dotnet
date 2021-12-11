ca8a9a3187a94872f247dd7a80ee5560f8a8e95b
pedrovbo <pedrovicboliveira@gmail.com>
2021-12-10 21:35:10 -0300

    add array GetLength study

--- decola-tech/ExemploColecoes/Colecoes/Program.cs
+++ decola-tech/ExemploColecoes/Colecoes/Program.cs
@@ -6,23 +6,42 @@ namespace Colecoes
     {
         static void Main(string[] args)
         {
-            int[] arrayInteiros = new int[3];
-
-            arrayInteiros[0] = 10;
-            arrayInteiros[1] = 20;
-            arrayInteiros[2] = 30;
-
-            System.Console.WriteLine("Percorrendo o array pelo For");
-            for (int i = 0; i < arrayInteiros.Length; i++)
+            int[,] matriz = new int[4, 2]
             {
-                System.Console.WriteLine(arrayInteiros[i]);
+                {8 , 8},
+                {10, 20}, 
+                {50, 100}, 
+                {90, 200}
+            };
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    System.Console.WriteLine(matriz[i, j]);
+                }
             }
 
-            System.Console.WriteLine("Percorrendo o array pelo ForEach");
-            foreach (int item in arrayInteiros)
-            {
-                System.Console.WriteLine(item);
-            }
+            // matriz[0, 0] = 5;
+
+            
+            // int[] arrayInteiros = new int[3];
+
+            // arrayInteiros[0] = 10;
+            // arrayInteiros[1] = 20;
+            // arrayInteiros[2] = int.Parse("30");
+
+            // System.Console.WriteLine("Percorrendo o array pelo For");
+            // for (int i = 0; i < arrayInteiros.Length; i++)
+            // {
+            //     System.Console.WriteLine(arrayInteiros[i]);
+            // }
+
+            // System.Console.WriteLine("Percorrendo o array pelo ForEach");
+            // foreach (int item in arrayInteiros)
+            // {
+            //     System.Console.WriteLine(item);
+            // }
         }
     }
 }
