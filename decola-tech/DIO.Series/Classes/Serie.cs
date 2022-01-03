9d36c2c9635136e9d750a29ef2f682c1f19f26ba
pedrovbo <pedrovicboliveira@gmail.com>
2022-01-03 16:25:31 -0300

    update project

---
+++ decola-tech/DIO.Series/Classes/Serie.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DIO.Series.Classes;
+
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        // Atributos
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        // Métodos
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+        }
+
+
+    }
+}
--- decola-tech/DIO.Series/Program.cs
+++ decola-tech/DIO.Series/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using DIO.Series.Classes;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EntidadeBase minhaClasse = new EntidadeBase();
+            Serie meuObjeto = new Serie();
+
+            
+        }
+    }
+}
