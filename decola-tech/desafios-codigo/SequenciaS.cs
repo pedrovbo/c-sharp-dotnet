b10352d26687934addd3fcf1f152d9efa57bb04f
pedrovbo <pedrovicboliveira@gmail.com>
2022-01-03 15:32:32 -0300

    add VaiTerCopa challenge

--- decola-tech/desafios-codigo/SequenciaS.cs
+++ decola-tech/desafios-codigo/SequenciaS.cs
@@ -1,20 +1,20 @@
-﻿using System;
+﻿//using System;
 
-class DIO
-{
+//class DIO
+//{
 
-    static void Main(string[] args)
-    {
+//    static void Main(string[] args)
+//    {
 
-        double a, c, S = 0;
-        for (a = 1; a <= 100; a++)
-        {
-            c = 1; 
-            S += c/a;
-        }
-        var x = Math.Round(S, 2);
-        Console.WriteLine(x);
+//        double a, c, S = 0;
+//        for (a = 1; a <= 100; a++)
+//        {
+//            c = 1; 
+//            S += c/a;
+//        }
+//        var x = Math.Round(S, 2);
+//        Console.WriteLine(x);
 
-    }
+//    }
 
-}
+//}
