880c4ef5d15f69c92d895eef9d8f4b6e96e1e1aa
pedrovbo <pedrovicboliveira@gmail.com>
2022-01-03 23:46:41 -0300

    add CRUD project

--- decola-tech/DIO.Series/Classes/Serie.cs
+++ decola-tech/DIO.Series/Classes/Serie.cs
@@ -14,6 +14,8 @@ namespace DIO.Series
         private string Descricao { get; set; }
         private int Ano { get; set; }
 
+        private bool Excluido { get; set; }
+
         // Métodos
         public Serie(int id, Genero genero, string titulo, string descricao, int ano)
         {
@@ -22,6 +24,7 @@ namespace DIO.Series
             Titulo = titulo;
             Descricao = descricao;
             Ano = ano;
+            Excluido = false;
         }
 
         public override string ToString()
@@ -30,7 +33,9 @@ namespace DIO.Series
             retorno += "Gênero: " + this.Genero + Environment.NewLine;
             retorno += "Título: " + this.Titulo + Environment.NewLine;
             retorno += "Descrição: " + this.Descricao + Environment.NewLine;
-            retorno += "Ano de Início: " + this.Ano;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluído: " + this.Excluido;
+
             return retorno;
         }
 
@@ -39,10 +44,19 @@ namespace DIO.Series
             return this.Titulo;
         }
 
-        internal int retornaId()
+        public int retornaId()
         {
             return this.Id;
         }
 
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
     }
 }
